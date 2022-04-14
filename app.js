0dfa1cb33de722426a1c163055cfb19475dee241
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-04-14 11:11:26 +0200

    finish mobile part of the exercise

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+const rates = document.querySelectorAll(".circle");
+const ratingSentence = document.querySelector(".rate-selected h2");
+const submit = document.querySelector('button');
 
 
-document.addEventListener("click", () => {
-	const rate = document.querySelector("circle");
-	console.log(rate);
+rates.forEach((rate) => {
+	rate.addEventListener('click', onClick, false);
 })
+
+function onClick(e) {
+	let rate = e.currentTarget;
+	document.querySelector("#rate").innerHTML = rate.innerText;
+}
+
+submit.addEventListener('click', (e) => {
+	console.log('clicking');
+	document.querySelector('.card').style.display = "none";
+	document.querySelector('.thanks-card ').style.display = "flex";
+
+
+})
+
